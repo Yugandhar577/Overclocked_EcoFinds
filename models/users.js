3242c0eb38c1201f5a0be2ee38d702055c5af5a8
Yugandhar577 <yug.paulbudhe@gmail.com>
2025-09-06 11:07:41 +0530

    updated users.js

--- models/users.js
+++ models/users.js
@@ -7,5 +7,18 @@ const userSchema = new Schema({
     password: { type: String, required: true },
 });
 
+// hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
+});
+
+// check password on login
+userSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+
 const User = mongoose.model('User', userSchema);
 module.exports = User;
