48015d4c0354453b610ff4276244de24b22b7eb9
Prithviraj Tandel <prithvirajtandel@gmail.com>
2025-09-06 11:50:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@
     const path = require('path');
     const methodOverride = require('method-override');
     const ejsMate = require('ejs-mate');
+    const session = require("express-session");
+    const MongoStore = require("connect-mongo");
+    const User = require("./Models/User"); 
 
     main().then(() =>{
         console.log("Connected to MongoDB");
@@ -22,6 +25,15 @@
     app.use(methodOverride("_method"));
     app.engine("ejs", ejsMate);
     app.use(express.static(path.join(__dirname, 'public')));
+
+    app.use(session({
+    secret: "ecoFindsSecretKey", // in production, use process.env.SESSION_SECRET
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: "mongodb://localhost:27017/FullStackAIRBNB" }), 
+    cookie: { maxAge: 1000 * 60 * 60 * 24 } // 1 day
+    }));
+
 
 
     app.get("/", (req, res) => {
