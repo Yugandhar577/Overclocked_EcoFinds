91708e141b363ab5bc969cb8e5a0a44851ffa863
Yugandhar577 <yug.paulbudhe@gmail.com>
2025-09-12 23:19:41 +0530

    some changes

---
+++ public/js/script.js
@@ -0,0 +1,17 @@
+// Bootstrap client-side validation
+(function () {
+    'use strict'
+
+    // Fetch all forms with class "needs-validation"
+    const forms = document.querySelectorAll('.needs-validation')
+
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
